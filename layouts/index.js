85c148163b607d64700f99fc68601ee3e5146a56
lubo1234 <552319293@qq.com>
2023-03-15 02:39:25 -0700

    初次提交

---
+++ layouts/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  webpackBarName: 'vue-admin-better',
+  webpackBanner:
+    ' build: vue-admin-better \n vue-admin-beautiful.com \n https://gitee.com/chu1204505056/vue-admin-better \n time: ',
+  donationConsole() {
+    const chalk = require('chalk')
+  },
+}
